d8ee4f18a4bc8afd4d88b38783ccb48b1464e445
Gaye Bulut <gayegul@gmail.com>
2015-11-22 10:34:42 -0800

    minor change

--- models/game.js
+++ models/game.js
@@ -3,7 +3,7 @@ var Schema = mongoose.Schema;
 var User = require('./user');
 
 var GameSchema = new Schema({
-  name: { type: String, required: true},
+  name: { type: String, required: true, index: { unique: true }},
   website: { type: String, required: true},
   user: { type: Schema.Types.ObjectId, ref: 'User', required: true } //foreign key
   // scoringtype: if low is good do smth, if high is good do smth else
